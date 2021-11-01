352944bf75f3d06a5bc2eb1fdca0603bd1be70b5
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-01 06:59:34 +0530

    ready

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card
-        date="28/20/2020"
-        logo="https://image.similarpng.com/very-thumbnail/2020/11/Amazon-icon-in-flat-design-on-transparent-background-PNG.png"
-        btn_text="Case Study"
-        heading="Amazon Gift Pay"
-        footer_text="Desktop - Mobile"
-      />
+      <div className="card" style={{ backgroundColor: "#fea501" }}>
+        <Card
+          date="28/20/2020"
+          logo="https://www.pngkey.com/png/full/480-4803237_amazon-icon-amazon-logo-png-icon.png"
+          btn_text="Case Study"
+          heading="Amazon Gift Pay"
+          footer_text="Desktop - Mobile"
+          icon="https://img.icons8.com/ios-glyphs/25/000000/long-arrow-right.png"
+        />
+      </div>
+      <div className="card" style={{ backgroundColor: "#f6f6f6" }}>
+        <Card
+          date="17 Sep 2020"
+          logo="https://www.transparentpng.com/thumb/apple-logo/d9RxbG-apple-logo-free-png.png"
+          btn_text="Case Study"
+          heading="Apple Gift Payment"
+          footer_text="MacOS - Mobile"
+          icon="https://img.icons8.com/ios-glyphs/25/000000/long-arrow-right.png"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,17 @@
-import Header from "./Header"
+import Header from "./Header";
+import "./css/card.css";
 
+let Card = ({ date, logo, btn_text, heading, footer_text, icon }) => {
+  return (
+    <>
+      <Header date={date} logo={logo} />
+      <br />
+      <button>{btn_text}</button>
+      <br />
+      <h2>{heading}</h2>
+      <Header date={footer_text} logo={icon} />
+    </>
+  );
+};
 
-let Card = ({ date , logo , btn_text , heading , footer_text }) => {
-    return (<div>
-        <Header date={date } logo={logo} />
-    
-    </div>)
-}
-
-export default Card
+export default Card;
